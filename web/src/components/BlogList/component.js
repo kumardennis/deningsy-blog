f9c1255cec8d0e60139f55cb31995925f45abed2
Dennis Kumar <dennis@lieturd.com>
2020-07-09 14:32:40 +0300

    added layouts and some fixes

--- web/src/components/BlogList/component.js
+++ web/src/components/BlogList/component.js
@@ -2,30 +2,27 @@ import React from 'react';
 import CardDeck from 'react-bootstrap/CardDeck';
 import BlogListItem from 'components/BlogListItem/component';
 import Container from 'react-bootstrap/Container';
+import BlockContent from '@sanity/block-content-to-react';
 
 import './styles.scss';
 
 const BlogList = ({query}) => {
+  const posts = query.allSanityPost.edges;
   return (
     <Container className='blog-list-container'>
       <CardDeck className='blog-list'>
-        <BlogListItem
-          title='Arrange a couch-bed in a single room apartment with ease!'
-          imgSrc='../../images/interior2-light.jpg'
-          author='Ingrid Kumar'
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam
-        </BlogListItem>
-        <BlogListItem
-          title='Arrange a couch-bed in a single room apartment with ease!'
-          imgSrc='../../images/interior3- dark.jpg'
-          author='Ingrid Kumar'
-        >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </BlogListItem>
+        {posts.map((post, id) => {
+          return (
+            <BlogListItem
+              title={post.node.title}
+              imgSrc={post.node.mainImage.asset.fluid.src}
+              author={post.node.authors[0].author.name}
+              slug={post.node.slug.current}
+            >
+              <BlockContent block={post.node._rawBody} />
+            </BlogListItem>
+          );
+        })}
       </CardDeck>
     </Container>
   );
