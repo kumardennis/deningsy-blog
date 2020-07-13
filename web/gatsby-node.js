c2b16f54c97aab4d216dab1cc2b66aa08b4e2b05
Dennis Kumar <dennis@lieturd.com>
2020-07-14 00:05:33 +0300

    background changes

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -21,7 +21,7 @@ exports.createPages = async ({graphql, actions}) => {
   result.data.allSanityPost.edges.forEach(({node}) => {
     createPage({
       path: node.categories[0].title.toLowerCase() + '/' + node.slug.current + '/',
-      component: path.resolve(`./src/templates/blog-post.js`),
+      component: path.resolve(`./src/templates/BlogPost/blog-post.js`),
       context: {
         slug: node.slug.current,
       },
